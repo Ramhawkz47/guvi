8c3c94f90f18e9c635551d659cd583d8150bff71
Sriram <sriram4700@gmail.com>
2019-04-12 23:46:45 +0530

    Update delnum.py

--- delnum.py
+++ delnum.py
@@ -1,8 +1,17 @@
+def deln(n,k):
+    if((n==0)or(k==0)):
+        return n
+    #print(n)
+    #print(k)
+    a=deln(n//10,k)*10+n%10
+    b=deln(n//10,k-1)
+    print(a)
+    print(b)
+    if a<b:
+        return a
+    else:
+        return b
+
 s=input()
 s=s.split(" ")
-#print(s)
-if(int(s[1])==0):
-    print(int(s[0]))
-else:
-    x=s[0][int(s[1]):]
-    print(x)
+print(deln(int(s[0]),int(s[1])))
