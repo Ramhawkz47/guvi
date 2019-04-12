882a4e1f315d37a9c0ef2dc0916acabbbfcf7487
Sriram <sriram4700@gmail.com>
2019-04-13 00:09:31 +0530

    Create 2s.py

---
+++ 2s.py
@@ -0,0 +1,22 @@
+x,y=input().split()
+m=len(x)
+n=len(y)
+if ((n>m)or(n==m)):
+    i=0
+    while i<m and x[i]==y[i]:
+        i+=1
+    print(n-i)
+else:
+    i=0
+    while i<n and x[i]==y[i]:
+        i+=1
+    a=x[i:]
+    b=y[i:]
+    l=list(a)
+    print(l)
+    j=0
+    for c in b:
+        if c in l:
+            j+=1
+            l.remove(c)
+    print(m-i-j)
