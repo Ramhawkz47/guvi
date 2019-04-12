5b3b540643b28aad7a0d41d06bf2da0ce3d0e0d4
Sriram <sriram4700@gmail.com>
2019-04-12 22:40:59 +0530

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,35 @@
+def larS(l):
+    x=len(l[0])
+    for i in l[1:]:
+        if(x>len(i)):
+            x=len(i)
+    return x
+
+n=int(input())
+l=[]
+for i in range(n):
+    l.append(input())
+l.sort()
+m=larS(l)
+s=""    
+d={}
+for i in range(m):
+    for j in range(len(l)):
+        try:
+            d[l[j][i]]+=1
+        except:
+            d[l[j][i]]=1
+
+    x=0
+    c=""
+    for j in d:
+        if(d[j]>=x):
+            x=d[j]
+            c=j
+    if(x==1):
+        break
+    #print(d)
+    s+=c
+    d={}
+
+print(s)
