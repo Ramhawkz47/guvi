45f4263822430194a2d2c69afb67df6e14e40604
Sriram <sriram4700@gmail.com>
2019-04-24 15:14:50 +0530

    Update rn.py

--- rn.py
+++ rn.py
@@ -1,3 +1,18 @@
+def remo(l,n):
+    l.sort()
+    x=0
+    m=[]
+    for i in l:
+        if(i==x):
+            m.append(x)
+
+        x=i
+    if((len(m)==n)or(n==2)):
+        return(m)
+    else:
+        #print(m)
+        return(remo(m,len(m)))
+
 n= int(input())
 l=input()
 l=l.split()
@@ -5,12 +20,7 @@ l=l.split()
 for i in range(len(l)):
   l[i]=int(l[i])
 
-l.sort()
-x=0
-m=[]
-for i in l:
-  if(i==x):
-    m.append(x)
-  x=i
+m=remo(l,n)
+
 for i in m:
   print(i,end=" ")
