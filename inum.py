17c93b906d05ade944e2db40653b8b37ae2667c8
Sriram <sriram4700@gmail.com>
2019-05-07 13:22:37 +0530

    Update inum.py

--- inum.py
+++ inum.py
@@ -2,8 +2,10 @@ x=input()
 s=input().split()
 l=[]
 for i in range(len(s)):
-    if(i==int(s[i])):
+    if((i==int(s[i]))):
         l.append(i)
+if(len(l)==0):
+    l.append(-1)
 
 for i in range(len(l)):
     if(i<len(l)-1):
